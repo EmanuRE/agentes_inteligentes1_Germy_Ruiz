9d145589f8613d4c812813914ac4700b650103d5
EmanuER <germyruiz10@gmail.com>
2025-03-01 17:23:32 -0600

    Finaliza el problema 1 : Agente de patrullaje (Reflejo Simple)

--- Problema1/agente_de_patrullaje_reflejo_simple.py
+++ Problema1/agente_de_patrullaje_reflejo_simple.py
@@ -7,23 +7,28 @@ class AgentePatrullaje:
         self.direcciones = ["este", "oeste", "norte", "sur"]  # Direcciones para cambiar
 
     def detectar_obstaculo(self):
-        """Simula la detección de un obstáculo."""
-        # Logica para el obstaculo
-        pass
+        """Se simula la detección de obstaculo"""
+        # Probabilidad de encontrar un obstaculo 20%
+        return random.random() < 0.2
 
     def cambiar_direccion(self):
-        """Cambia la dirección del agente de manera aleatoria."""
-        # Logica para el cambio de dirección
-        pass
+        """Cambio de dirección de forma aleatoria"""
+        return random.choice(self.direcciones)
 
     def patrullar(self):
-        """Patrulla la ruta predefinida."""
-        # Logica para patrullaje
-        pass
+        """Patrulla para recorrer la ruta"""
+        while self.posicion_actual < len(self.ruta):
+            print(f"Posición actual: {self.ruta[self.posicion_actual]}")
+            if self.detectar_obstaculo():
+                nueva_direccion = self.cambiar_direccion()
+                print(f"¡Un obstáculo detectado! Cambio de dirección a → {nueva_direccion}")
+            else:
+                print("Avanzando a la siguiente ruta")
+                self.posicion_actual += 1
 
-# Rutas
+# Rutas donde pasara
 ruta = ["Punto 1", "Punto 2", "Punto 3", "Punto 4", "Punto 5"]
 
-# Creación y ejecución del agente
+# Creación y ejecucion del agente
 agente = AgentePatrullaje(ruta)
 agente.patrullar()
